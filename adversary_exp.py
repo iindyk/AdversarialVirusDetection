7e802e811f8998b274ed1752cc5e9ecb3ab48f3d
iindyk <ihor.indyk@gmail.com>
2018-09-18 15:54:34 -0400

    new adversary problem solution start

--- adversary_exp.py
+++ adversary_exp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import datetime
+import cvxpy as cvx
 
 
 class Adversary:
@@ -18,9 +19,22 @@ class Adversary:
         self.train_dataset = np.append(self.train_dataset, new_train_data, axis=0)
         self.train_labels = np.append(self.train_labels, new_train_labels)
 
+    def get_support_points_split(self, dataset):
+        support_ineq = dataset[:len(dataset)//3]
+        support_eq = dataset[len(dataset)//3 : 2*len(dataset)//3]
+        non_support = dataset[2*len(dataset)//3:]
+        return [[support_ineq, support_eq, non_support]]
+
     # @profile
     def get_infected_dataset(self, new_train_data, new_train_labels):
         n, m = np.shape(new_train_data)
-        x0 = np.zeros(m + 1 + n * m)
 
-        def decompose_x(x, )
+        for support_ineq, support_eq, non_support in self.get_support_points_split(new_train_data):
+            w = cvx.Variable(m)
+            b = cvx.Variable()
+            l = cvx.Variable(len(support_eq))
+            g = cvx.Variable(n)
+
+            obj = cvx.Minimize(sum([max(-1.0, new_train_labels[i]*(w*new_train_data[i]+b)) for i in range(n)])/n)
+
+
--- svm_toy.py
+++ svm_toy.py
@@ -1,18 +1,17 @@
-from sklearn.svm import SVC
-from random import random
-
-
-n = 1000
-dataset = []
-labels = []
-for i in range(n):
-    new_x = random()
-    new_y = random()
-    dataset.append([new_x, new_y])
-    labels.append(1 if new_x+new_y > 1 else -1)
-
-svc = SVC(kernel='linear')
-svc.fit(dataset, labels)
-print(svc.dual_coef_)
-print(svc.n_support_)
-print(svc.support_)
+import cvxpy as cvx
+# Create two scalar optimization variables.
+x = cvx.Variable()
+y = cvx.Variable()
+# Create two constraints.
+constraints = [x + y == 1,
+x - y >= 1]
+# Form objective.
+obj = cvx.Minimize((x - y)**2)
+# Form and solve problem.
+prob = cvx.Problem(obj, constraints)
+prob.solve()
+# The optimal dual variable (Lagrange multiplier) for
+# a constraint is stored in constraint.dual_value.
+print("optimal (x + y == 1) dual variable", constraints[0].dual_value)
+print("optimal (x - y >= 1) dual variable", constraints[1].dual_value)
+print("x - y value:", (x - y).value)
