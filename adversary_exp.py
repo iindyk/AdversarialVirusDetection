ba19cbaa43bb470e5e7fc9c6307598a491a6fbc6
iindyk <ihor.indyk@gmail.com>
2018-09-18 19:52:12 -0400

    new adversary problem solution start

--- adversary_exp.py
+++ adversary_exp.py
@@ -2,6 +2,8 @@ import numpy as np
 import datetime
 import cvxpy as cvx
 
+from sklearn.svm import SVC
+
 
 class Adversary:
     eps = 0.1
@@ -19,22 +21,59 @@ class Adversary:
         self.train_dataset = np.append(self.train_dataset, new_train_data, axis=0)
         self.train_labels = np.append(self.train_labels, new_train_labels)
 
-    def get_support_points_split(self, dataset):
-        support_ineq = dataset[:len(dataset)//3]
-        support_eq = dataset[len(dataset)//3 : 2*len(dataset)//3]
-        non_support = dataset[2*len(dataset)//3:]
-        return [[support_ineq, support_eq, non_support]]
+    def get_support_points_split(self, n):
+        # todo: come up with a better algorithm
+        if n == 1:
+            yield [[0], [], []]
+            yield [[], [0], []]
+            yield [[], [], [0]]
+            raise StopIteration
+        else:
+            for support_ineq, support_eq, non_support in self.get_support_points_split(n-1):
+                yield [support_ineq+[n-1], support_eq, non_support]
+                yield [support_ineq, support_eq+[n-1], non_support]
+                yield [support_ineq, support_eq, non_support+[n-1]]
 
     # @profile
     def get_infected_dataset(self, new_train_data, new_train_labels):
         n, m = np.shape(new_train_data)
+        svc = SVC(C=self.a)
+        svc.fit(new_train_data, new_train_labels)
+        w_old = svc.coef_[0]
+        b_old = svc.intercept_
 
         for support_ineq, support_eq, non_support in self.get_support_points_split(new_train_data):
+            sup_ineq_data = new_train_data[support_ineq]
+            sup_ineq_labels = new_train_labels[support_ineq]
+            sup_eq_data = new_train_data[support_eq]
+            sup_eq_labels = new_train_labels[support_eq]
+            non_sup_data = new_train_data[non_support]
+            non_sup_labels = new_train_labels[non_support]
+
             w = cvx.Variable(m)
             b = cvx.Variable()
             l = cvx.Variable(len(support_eq))
             g = cvx.Variable(n)
+            z = cvx.Variable(n)
 
-            obj = cvx.Minimize(sum([max(-1.0, new_train_labels[i]*(w*new_train_data[i]+b)) for i in range(n)])/n)
+            obj = cvx.Minimize(cvx.sum(z)/n)
 
+            cons = [z >= -1,
+                    l >= 0,
+                    l <= self.a,    # todo: double check
+                    w == cvx.sum([l[i]*sup_eq_labels[i]*sup_eq_data[i] for i in len(support_eq)] +
+                                 sum([self.a*sup_ineq_labels[i]*sup_ineq_data[i] for i in len(support_ineq)])),
+                    l*sup_eq_labels + self.a*sum(sup_ineq_labels) == 0,
+                    cvx.norm(g) <= self.eps,
+                    w*w_old >= 0.5]
+            for i in range(n):
+                cons.append(z[i] >= new_train_labels[i]*(w*new_train_data[i]+b))
+                if i in support_ineq:
+                    cons.append(new_train_labels[i]*(w*new_train_data[i]+g[i]+b) <= 1)
+                elif i in support_eq:
+                    cons.append(new_train_labels[i] * (w * new_train_data[i] + g[i] + b) == 1)
+                else:
+                    cons.append(new_train_labels[i] * (w * new_train_data[i] + g[i] + b) >= 1)
 
+            prob = cvx.Problem(obj, cons)
+            prob.solve()
--- svm_toy.py
+++ svm_toy.py
@@ -1,17 +1,4 @@
 import cvxpy as cvx
-# Create two scalar optimization variables.
-x = cvx.Variable()
-y = cvx.Variable()
-# Create two constraints.
-constraints = [x + y == 1,
-x - y >= 1]
-# Form objective.
-obj = cvx.Minimize((x - y)**2)
-# Form and solve problem.
-prob = cvx.Problem(obj, constraints)
-prob.solve()
-# The optimal dual variable (Lagrange multiplier) for
-# a constraint is stored in constraint.dual_value.
-print("optimal (x + y == 1) dual variable", constraints[0].dual_value)
-print("optimal (x - y >= 1) dual variable", constraints[1].dual_value)
-print("x - y value:", (x - y).value)
+import adversary_exp as adv
+
+adv = adv.Adversary([], [], [], [], 2)
